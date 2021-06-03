2fe2edb54f94a229637839a2bdba0ec891eebf18
Anthony <71753504+AnthonyAich@users.noreply.github.com>
2021-06-04 01:39:58 +0200

    Fix navbar taal en width

--- assets/js/browseMappen.js
+++ assets/js/browseMappen.js
@@ -1,16 +1,21 @@
 'use strict';
 
 let scherm = {
-  		actief: ".bier",
-  		container: ".bieren",
-  		taal: "nl"
+    actief: ".bier",
+    container: ".bieren",
+    taal: "nl"
 };
 
 //nav en containers
-let bierArray = ['.bier','.bieren'];
-let hapjesArray = ['.hapje','.hapjes'];
-let frisdrankenArray = ['.frisdrank','.frisdrankenContent'];
-let warmArray = ['.warm','.warmContent'];
+let bierArray = ['.bier', '.bieren'];
+let hapjesArray = ['.hapje', '.hapjes'];
+let frisdrankenArray = ['.frisdrank', '.frisdrankenContent'];
+let warmArray = ['.warm', '.warmContent'];
+
+//taal nav
+let nlNav = ['Bieren', 'Frisdranken', 'warme Dranken', 'Hapjes'];
+let frNav = ['Bières', 'Boissons sans alcool', 'Boissons chaudes', 'Snacks'];
+let enNav = ['Beers', 'Soft Drinks', 'Hot Drinks', 'Snacks'];
 
 function bier() {
     document.querySelector(scherm.actief).classList.remove('actief');
@@ -19,14 +24,14 @@ function bier() {
     scherm.actief = bierArray[0];
 }
 
-function hapjes() {    
+function hapjes() {
     document.querySelector(scherm.actief).classList.remove('actief');
     document.querySelector(hapjesArray[0]).classList.add('actief');
 
     scherm.actief = hapjesArray[0];
 }
 
-function frisdrank() {    
+function frisdrank() {
     document.querySelector(scherm.actief).classList.remove('actief');
     document.querySelector(frisdrankenArray[0]).classList.add('actief');
 
@@ -46,9 +51,30 @@ function init() {
     document.querySelector('.hapje').addEventListener("click", hapjes);
     document.querySelector('.frisdrank').addEventListener("click", frisdrank);
 
-fetch('assets/js/apiMenu.json')
-.then(res => res.json())
-.then(data => console.log(data));
+    if (scherm.taal === 'nl') {
+        document.querySelector('.bier').innerHTML = nlNav[0];
+        document.querySelector('.warm').innerHTML = nlNav[1];
+        document.querySelector('.hapje').innerHTML = nlNav[2];
+        document.querySelector('.frisdrank').innerHTML = nlNav[3];
+    }
+
+    if (scherm.taal === 'fr') {
+        document.querySelector('.bier').innerHTML = frNav[0];
+        document.querySelector('.warm').innerHTML = frNav[1];
+        document.querySelector('.hapje').innerHTML = frNav[2];
+        document.querySelector('.frisdrank').innerHTML = frNav[3];
+    }
+
+    if (scherm.taal === 'en') {
+        document.querySelector('.bier').innerHTML = enNav[0];
+        document.querySelector('.warm').innerHTML = enNav[1];
+        document.querySelector('.hapje').innerHTML = enNav[2];
+        document.querySelector('.frisdrank').innerHTML = enNav[3];
+    }
+
+    fetch('assets/js/apiMenu.json')
+        .then(res => res.json())
+        .then(data => console.log(data));
 
 }
 window.onload = init;
