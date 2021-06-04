b49779c791ff316b541e3745e3ee6d6ce5b8344b
Anthony <71753504+AnthonyAich@users.noreply.github.com>
2021-06-05 00:09:49 +0200

    taal + api

--- assets/js/browseMappen.js
+++ assets/js/browseMappen.js
@@ -1,9 +1,11 @@
 'use strict';
 
+//schermInfo
 let scherm = {
     actief: ".bier",
-    container: ".bieren",
-    taal: "nl"
+    container: "bieren",
+    taal: "nl",
+    nummer: 0
 };
 
 //nav en containers
@@ -17,6 +19,11 @@ let nlNav = ['Bieren', 'Frisdranken', 'warme Dranken', 'Hapjes'];
 let frNav = ['Bières', 'Boissons sans alcool', 'Boissons chaudes', 'Snacks'];
 let enNav = ['Beers', 'Soft Drinks', 'Hot Drinks', 'Snacks'];
 
+//api URL's
+const bierAPI = ['assets/js/bierAPI.json']
+const warmAPI = ['assets/js/warmAPI.json']
+
+//klikken op navbar //!(begin)
 function bier() {
     document.querySelector(scherm.actief).classList.remove('actief');
     document.querySelector(bierArray[0]).classList.add('actief');
@@ -44,6 +51,100 @@ function warm() {
 
     scherm.actief = warmArray[0];
 }
+//klikken op navbar //!(einde)
+
+
+//initialiseren nav voor geselecteerde taal
+function navTaal() {
+    if (scherm.taal === 'nl') {
+        document.querySelector('.bier').innerHTML = nlNav[0];
+        document.querySelector('.warm').innerHTML = nlNav[2];
+        document.querySelector('.hapje').innerHTML = nlNav[3];
+        document.querySelector('.frisdrank').innerHTML = nlNav[1];
+    }
+
+    if (scherm.taal === 'fr') {
+        document.querySelector('.bier').innerHTML = frNav[0];
+        document.querySelector('.warm').innerHTML = frNav[2];
+        document.querySelector('.hapje').innerHTML = frNav[3];
+        document.querySelector('.frisdrank').innerHTML = frNav[1];
+    }
+
+    if (scherm.taal === 'en') {
+        document.querySelector('.bier').innerHTML = enNav[0];
+        document.querySelector('.warm').innerHTML = enNav[2];
+        document.querySelector('.hapje').innerHTML = enNav[3];
+        document.querySelector('.frisdrank').innerHTML = enNav[1];
+    }
+}
+
+
+//vlag geselecteerde taal setten
+function vlagGeselecteerdeTaal() {
+
+    if (scherm.taal === 'nl') {
+        document.querySelector('.geselecteerdeTaal').src = 'assets/images/be.png';
+    }
+
+    if (scherm.taal === 'fr') {
+        document.querySelector('.geselecteerdeTaal').src = 'assets/images/fr.png';
+    }
+
+    if (scherm.taal === 'en') {
+        document.querySelector('.geselecteerdeTaal').src = 'assets/images/en.png';
+    }
+
+}
+
+function taalAanpassenSchermpje() {
+    if(scherm.nummer === 0) {
+
+    document.querySelector('.taal').style.height = "105px";
+    document.querySelector('.taal').style.borderRadius = "20px";
+
+    let vlaggen;
+    scherm.nummer = 1;
+    if (scherm.taal === 'nl') {
+        vlaggen = ` <img class="geselecteerdeTaal nederlandsK" onclick="taalAanpassen('nl')"  src="assets/images/be.png" alt="" />
+        <img src="assets/images/ArrowDown.png" alt="" />
+        <img class="geselecteerdeTaal engelsK" onclick="taalAanpassen('en')" src="assets/images/en.png" alt="" />
+        <img src="" alt="" />
+        <img class="geselecteerdeTaal fransK" src="assets/images/fr.png" onclick="taalAanpassen('fr')" alt="" />
+        <img src="" alt="" />`;
+    }
+
+    if (scherm.taal === 'en') {
+        vlaggen = ` <img class="geselecteerdeTaal engelsK" onclick="taalAanpassen('en')" src="assets/images/en.png" alt="" />
+        <img src="assets/images/ArrowDown.png" alt="" />
+        <img class="geselecteerdeTaal nederlandsK" onclick="taalAanpassen('nl')" src="assets/images/be.png" alt="" />
+        <img src="" alt="" />
+        <img class="geselecteerdeTaal fransK" onclick="taalAanpassen('fr')" src="assets/images/fr.png" alt="" />
+        <img src="" alt="" />`;
+    }
+
+    if (scherm.taal === 'fr') {
+        vlaggen = ` <img class="geselecteerdeTaal fransK" onclick="taalAanpassen('fr')" src="assets/images/fr.png" alt="" />
+        <img src="assets/images/ArrowDown.png" alt="" />
+        <img class="geselecteerdeTaal nederlandsK" onclick="taalAanpassen('nl')" src="assets/images/be.png" alt="" />
+        <img src="" alt="" />
+        <img class="geselecteerdeTaal engelsK" onclick="taalAanpassen('en')" src="assets/images/en.png" alt="" />
+        <img src="" alt="" />`;
+    }
+
+    document.querySelector('.taal').innerHTML = vlaggen;
+ }
+}
+
+function taalAanpassen(taal) {
+    let vlag = '<img class="geselecteerdeTaal" src="assets/images/be.png" alt="" > <img src="assets/images/ArrowDown.png" alt="" />';
+    document.querySelector('.taal').innerHTML = vlag;
+    document.querySelector('.taal').style.height = "35px";
+    document.querySelector('.taal').style.borderRadius = "40px";
+    scherm.taal = taal;
+    init();
+      setTimeout(function(){ scherm.nummer = 0; }, 1500);
+
+}
 
 function init() {
     document.querySelector('.bier').addEventListener("click", bier);
@@ -51,30 +152,75 @@ function init() {
     document.querySelector('.hapje').addEventListener("click", hapjes);
     document.querySelector('.frisdrank').addEventListener("click", frisdrank);
 
-    if (scherm.taal === 'nl') {
-        document.querySelector('.bier').innerHTML = nlNav[0];
-        document.querySelector('.warm').innerHTML = nlNav[1];
-        document.querySelector('.hapje').innerHTML = nlNav[2];
-        document.querySelector('.frisdrank').innerHTML = nlNav[3];
+    document.querySelector('.taal').addEventListener("click", taalAanpassenSchermpje);
+
+    navTaal();
+    vlagGeselecteerdeTaal();
+
+    // fetch('assets/js/apiMenu.json')
+    //     .then(res => res.json())
+    //     .then(data => {let lengte = data.data.length;});
+    //     console.log(lengte);
+
+
+// api url
+
+//taal voor API Def
+let taalAPInr;
+if(scherm.taal === 'nl') {
+    taalAPInr = 0;
+}
+if(scherm.taal === 'fr') {
+    taalAPInr = 1;
+}
+if(scherm.taal === 'en') {
+    taalAPInr = 2;
+}
+let api_url;
+if(scherm.container === 'bieren') {
+    api_url = bierAPI[taalAPInr];
+}
+if(scherm.container === 'warm') {
+    api_url = warmAPI[taalAPInr];
+}
+
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    let response = await fetch(url);
+    
+    // Storing data in form of JSON
+    let data = await response.json();
+    console.log(data);
+    if (response) {
     }
-
-    if (scherm.taal === 'fr') {
-        document.querySelector('.bier').innerHTML = frNav[0];
-        document.querySelector('.warm').innerHTML = frNav[1];
-        document.querySelector('.hapje').innerHTML = frNav[2];
-        document.querySelector('.frisdrank').innerHTML = frNav[3];
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = ""
+    
+    // Loop to access all rows 
+    for (let r of data.data) {
+        tab += `      <div class="item">
+        <div class="foto">
+          <img src="${r.img}" alt="" />
+        </div>
+        <div class="info">
+          <p class="titel">${r.naam}</p>
+          <p class="graden">(${r.graden}% vol.)</p>
+          <p class="prijs">€ ${r.prijs}</p>
+        </div>
+      </div>`;
     }
-
-    if (scherm.taal === 'en') {
-        document.querySelector('.bier').innerHTML = enNav[0];
-        document.querySelector('.warm').innerHTML = enNav[1];
-        document.querySelector('.hapje').innerHTML = enNav[2];
-        document.querySelector('.frisdrank').innerHTML = enNav[3];
-    }
-
-    fetch('assets/js/apiMenu.json')
-        .then(res => res.json())
-        .then(data => console.log(data));
-
+    // Setting innerHTML as tab variable
+    document.querySelector(".container").innerHTML = tab;
+}
 }
 window.onload = init;
